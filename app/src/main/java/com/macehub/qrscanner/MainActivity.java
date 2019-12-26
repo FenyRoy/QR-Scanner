a60b1405068c886dbc1d46fb188bd94f1ace7e84
Feny Roy <fenyroy@gmail.com>
2019-12-26 19:06:09 +0530

    Progress #4

--- app/src/main/java/com/macehub/qrscanner/MainActivity.java
+++ app/src/main/java/com/macehub/qrscanner/MainActivity.java
@@ -4,14 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText EmailEdt;
+    EditText PassEdt;
+    Button LoginBtn;
+    String email,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
+        EmailEdt = findViewById(R.id.login_user);
+        PassEdt = findViewById(R.id.login_password);
+        LoginBtn = findViewById(R.id.login_btn);
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email=EmailEdt.getText().toString();
+                pass=PassEdt.getText().toString();
+
+                if((email.equals("Feny@mace.in"))&&(pass.equals("1234mace"))){
+
+                    startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
+                    
+                }
+
+            }
+        });
+
     }
 }
